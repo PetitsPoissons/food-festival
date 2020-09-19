c2d22465cbe9965897eb3d8181de4b46e1d4e1cd
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-09-19 12:43:42 -0700

    add webpack plugin

--- webpack.config 3.js
+++ webpack.config 3.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
   entry: "./assets/js/script.js",
@@ -7,11 +6,5 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.bundle.js",
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-    }),
-  ],
   mode: "development",
 };
