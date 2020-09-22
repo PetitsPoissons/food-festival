205be3e965965e26c8c09ce7609d70280c98b9be
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-09-22 07:51:09 -0700

    lesson 3 underway

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: "./assets/js/script.js",
+  entry: './assets/js/script.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js',
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // the report outputs to an html file in the dist/ folder
     }),
   ],
-  mode: "development",
+  mode: 'development',
 };
